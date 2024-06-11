f0e1f13c6588c390ef66e3734d571e7ccf1a5667
finstory <facu995electro@hotmail.com>
2024-06-11 17:32:29 -0300

    change endpoind protected

--- dist/routes/auth/protect.routes.js
+++ dist/routes/auth/protect.routes.js
@@ -5,17 +5,24 @@ const express_1 = require("express");
 const isLoggedIn_1 = require("../../middleware/isLoggedIn");
 const router = (0, express_1.Router)();
 exports.protectedRouter = router;
-router.get('/protected', isLoggedIn_1.isLoggedIn, (req, res) => {
+router.get("/protected", isLoggedIn_1.isLoggedIn, (req, res) => {
     const user = req.user; // Hacemos una conversiÃ³n de tipo
-    if (!user || !user.photos || !user.photos.length || !user.emails || !user.emails.length || !user.provider) {
-        return res.status(400).send('User profile is incomplete.');
+    if (!user ||
+        !user.photos ||
+        !user.photos.length ||
+        !user.emails ||
+        !user.emails.length ||
+        !user.provider) {
+        res.status(400).send("User profile is incomplete.");
     }
-    res.send(`
-      <img width=80 src="${user.photos[0].value}" />
-      <h1>Hello ${user.displayName}</h1>
-      <h4>Your email: ${user.emails[0].value}</h4>
-      <h4>Your provider: ${user.provider}</h4>
-      <a href="/logout">Logout</a>
-    `);
+    else
+        res.status(200).json(user);
+    // res.send(`
+    //     <img width=80 src="${user.photos[0].value}" />
+    //     <h1>Hello ${user.displayName}</h1>
+    //     <h4>Your email: ${user.emails[0].value}</h4>
+    //     <h4>Your provider: ${user.provider}</h4>
+    //     <a href="/logout">Logout</a>
+    //   `);
 });
 //# sourceMappingURL=protect.routes.js.map
