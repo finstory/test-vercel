d7814d79a8bb7c20d975bcdb3d848783b6eccc5a
Ramiro <ramiroignacio_14@hotmail.com>
2024-06-13 01:34:56 -0300

    rama 07

--- dist/routes/auth/protect.routes.js
+++ dist/routes/auth/protect.routes.js
@@ -2,9 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.protectedRouter = void 0;
 const express_1 = require("express");
+const crypto_middleware_1 = require("../../middleware/crypto/crypto.middleware");
 const router = (0, express_1.Router)();
 exports.protectedRouter = router;
 router.get('/protected', (req, res) => {
+    const randomValue = (0, crypto_middleware_1.generateRandomValue)(10);
+    res.cookie("cookie", randomValue, {
+        maxAge: 3600000,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax'
+    });
     if (req.isAuthenticated()) {
         // Obtener la cookie
         const cookieValue = req.cookies.cookie;
--- src/routes/auth/protect.routes.ts
+++ src/routes/auth/protect.routes.ts
@@ -2,10 +2,19 @@ import { IRouter, Router, Request, Response } from "express";
 import { IUser } from "../../database/mongo/model/User.model";
 import { isLoggedIn } from "../../middleware/isLoggedIn";
 import { logoutGet } from "../../controller/auth/auth.controller";
+import { generateRandomValue } from "../../middleware/crypto/crypto.middleware";
 
 const router: IRouter = Router();
 
 router.get('/protected', (req, res) => {
+    const randomValue = generateRandomValue(10);
+    res.cookie("cookie", randomValue, { 
+        maxAge: 3600000, 
+        httpOnly: true, 
+        secure: true, 
+        sameSite: 'lax' 
+    });
+  
     if (req.isAuthenticated()) {
         // Obtener la cookie
         const cookieValue = req.cookies.cookie;
