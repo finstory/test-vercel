618420095c063ca629cc83bf1b8ea4b1af67b5e0
Facundo Alvarez <95634052+finstory@users.noreply.github.com>
2024-11-25 04:04:18 -0300

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,14 @@
+export const colors = () => {
+    return {
+        primary: "#76C49A",
+    }
+}
+
+export const __FramerMetadata__ = {
+    exports: {
+        colors: { type: "variable", annotations: { framerContractVersion: "1" } },
+        __FramerMetadata__: { type: "variable" },
+    },
+};
+
+//# sourceMappingURL=./colors.map
