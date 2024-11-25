7b67119ca8ef4a60337cd96370d2cfed03eb5c18
Facundo Alvarez <95634052+finstory@users.noreply.github.com>
2024-11-25 03:09:55 -0300

    Update useFetch.js

--- useFetch.js
+++ useFetch.js
@@ -1,24 +1,19 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 export const useFetch = () => {
-  const [name, setName] = useState("facu");
-  useEffect(() => {
-    getName();
-  }, []);
-  const getName = async () => {
-    let name = "Facundo Alvarez";
-    const response = await axios.get(
-      "https://random-word-api.herokuapp.com/word"
-    );
-    setName(response.data);
-  };
-  return name;
-};
-export const __FramerMetadata__ = {
-  exports: {
-    useFetch: { type: "variable", annotations: { framerContractVersion: "1" } },
-    __FramerMetadata__: { type: "variable" },
-  },
-};
-//# sourceMappingURL=./useFetch.map
+    const [name, setName] = useState<string>("juan")
+    useEffect(() => {
+        getName()
+    }, [])
+
+    const getName = async () => {
+        let name = "Facundo Alvarez"
+        const response = await axios.get(
+            "https://random-word-api.herokuapp.com/word"
+        )
+        setName(response.data)
+    }
+
+    return name
+}
